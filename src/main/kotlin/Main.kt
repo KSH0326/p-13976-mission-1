b66c23de85b80abf78c757cafcc7420006b372fc
KSH0326 <ths0074@naver.com>
2025-08-11 15:40:32 +0900

    초기세팅

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+package com
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    println(" == 명언 앱 == ")
+    while(true){
+        print("명령) ")
+        val input = readlnOrNull()
+
+        if(input == "종료")
+            break
+
+    }
+}
